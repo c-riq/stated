dc289876ac3db9baf0cc8366d03b7c34b8df09a5
Chris Rieckmann <chrisrieck6@gmail.com>
2022-11-05 18:40:31 +0100

    refactor api

--- backend/api.js
+++ backend/api.js
@@ -10,6 +10,7 @@ const statementVerification = require('./statementVerification');
 var api = express.Router();
 const apiVersion = '1'
 
+api.use((req, res, next) => res.setHeader('Content-Type', 'application/json'))
 
 api.post("/get_txt_records", (req, res, next) => {
     try {
@@ -19,9 +20,7 @@ api.post("/get_txt_records", (req, res, next) => {
                 console.log(req.body)
                 statementVerification.getTXTEntries(req.body.domain)
                     .then(records => {
-                        res.setHeader('Content-Type', 'application/json');
                         res.end(JSON.stringify({ records: records }));
-                        res.end()
                     })
                     .catch(next);
             })
@@ -29,51 +28,20 @@ api.post("/get_txt_records", (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-});
+})
 
 
 api.post("/submit_statement", async (req, res, next) => {
-    try {
-        const { content, hash, domain, time, statement, type, content_hash } = req.body
-        console.log("req.body",req.body)
-        if (await hashUtils.verify(req.body.statement, req.body.hash)) {
-            txtCorrect = await statementVerification.verifyTXTRecord("stated." + req.body.domain, req.body.hash)
-            console.log("txtCorrect", txtCorrect)
-            if (txtCorrect) {
-                try {
-                    var dbResult = await db.createStatement({type, version: 1, domain, statement, time, 
+    const { content, hash, domain, time, statement, type, content_hash } = req.body
+    console.log("req.body",req.body)
+    const dbResult = await statementVerification.validateAndAddStatementIfMissing({type, version: 1, domain, statement, time, 
                         hash_b64: hash, content, content_hash, verification_method: 'dns'})
-                    if(dbResult?.error){
-                        throw dbResult?.error
-                    }
-                    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-                    res.end(JSON.stringify({ insertedData: dbResult }));
-                }
-                catch (e){
-                    console.log(e,"3")
-                    res.status(500).send({
-                        message: 'Could not save statement'
-                     })
-                }
-            } else {
-                return res.status(400).send({
-                    message: 'Could not verify TXT records'
-                })
-            }
-        } else {
-            return res.status(400).send({
-                message: 'Could not verify hash'
-             })
-        }
-    } catch (err) {
-        console.log(err)
-        return res.status(400).send({
-            message: 'Could not verify hash'
-         })
+    if(dbResult?.error){
+        res.status(400).send({message: dbResult.error})
     }
-});
-
-
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify({ insertedData: dbResult }));
+})
 
 api.get("/statements", async (req, res, next) => {
     console.log(req.query)
@@ -87,7 +55,8 @@ api.get("/statements", async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-});
+})
+
 api.post("/statement", async (req, res, next) => {
     try {
         const dbResult = await db.getStatement({hash_b64: req.body.hash_b64})
@@ -98,7 +67,8 @@ api.post("/statement", async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-});
+})
+
 api.post("/verifications", async (req, res, next) => {
     try {
         console.log(req.body.domain, req.body)
@@ -110,19 +80,16 @@ api.post("/verifications", async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-});
+})
+
 api.post("/joining_statements", async (req, res, next) => {
-    try {
-        console.log(req.body.content_hash, req.body)
-        const dbResult = await db.getJoiningStatements({hash_b64: req.body.hash_b64})
-        if(dbResult?.error){
-            throw dbResult?.error
-        }
-        res.end(JSON.stringify({statements: dbResult.rows, time: new Date().toUTCString()}))       
-    } catch (err) {
-        return next(err);
+    const dbResult = await db.getJoiningStatements({hash_b64: req.body.hash_b64})
+    if(dbResult?.error){
+        next(dbResult.error)
     }
-});
+    res.end(JSON.stringify({statements: dbResult.rows, time: new Date().toUTCString()}))       
+})
+
 api.get("/nodes", async (req, res, next) => {
     try {
         const dbResult = await db.getAllNodes()
@@ -134,33 +101,24 @@ api.get("/nodes", async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-});
+})
+
 api.post("/join_network", async (req, res, next) => {
-    try {
-        console.log(req.body)
-        const r = await p2p.validateAndAddNode({domain: req.body.domain})
-        if (r.error) {
-            next(r.error)
-        } else {
-            res.end(JSON.stringify(r))
-        }
-    } catch (err) {
-        return next(err);
+    const r = await p2p.validateAndAddNode({domain: req.body.domain})
+    if(r?.error){
+        next(r?.error)
+    } else {
+        res.end(JSON.stringify(r))
     }
-});
+})
 
 api.get("/health", async (req, res, next) => {
-    try {
-        // check if database is working
-        const dbResult = await db.getAllNodes()
-        if(dbResult?.error){
-            throw dbResult?.error
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ apiVersion, application: "stated" }));   
-    } catch (err) {
-        return next(err);
+    const dbResult = await db.getAllNodes()
+    if(dbResult?.error){
+        next(dbResult?.error)
+    } else {
+        res.end(JSON.stringify({ apiVersion, application: "stated" }))
     }
-});
+})
 
 module.exports = api;
