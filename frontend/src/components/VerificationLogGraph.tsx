f9b5add07844caa24127f7fe0f21ea9fe9579d0e
Chris Rieckmann <chrisrieck6@gmail.com>
2023-10-08 16:45:00 +0200

    Fix verification monitoring default domain

--- frontend/src/components/VerificationLogGraph.tsx
+++ frontend/src/components/VerificationLogGraph.tsx
@@ -20,8 +20,8 @@ const getMonday = (d: Date) => {
 }
 
 const getWeeklyTimeSeries = (data: { result: vLog[] }): vLogWeekly[] => {
-  const withWeeks: (vLog & {w: string})[] = data.result.map(i => ({ ...i, w: getMonday(new Date(i.t)).toString() }))
-  const weekly: {[key: string]: vLogWeeklyFields} = withWeeks.reduce((acc: {[key: string]: vLogWeeklyFields}, i: vLog & {w: string}) => {
+  const withWeeks: (vLog & { w: string })[] = data.result.map(i => ({ ...i, w: getMonday(new Date(i.t)).toString() }))
+  const weekly: { [key: string]: vLogWeeklyFields } = withWeeks.reduce((acc: { [key: string]: vLogWeeklyFields }, i: vLog & { w: string }) => {
     const _i = { ...i, api: i.api ? 1 : 0, dns: i.dns ? 1 : 0, txt: i.txt ? 1 : 0 }
     if (!acc[_i.w]) {
       acc[i.w] = { api: _i.api, dns: _i.dns, txt: _i.txt, count: 1 }
@@ -33,7 +33,7 @@ const getWeeklyTimeSeries = (data: { result: vLog[] }): vLogWeekly[] => {
       acc[_i.w]['count'] += 1
     }
     return acc
-  }, {} as {[key: string]: vLogWeeklyFields})
+  }, {} as { [key: string]: vLogWeeklyFields })
   const weeklySeries = Object.keys(weekly).map(i => ({ ...weekly[i], t: new Date(i) }))
   return weeklySeries
 }
@@ -66,7 +66,7 @@ const valueFormatter = (date: Date) =>
 const VerificationLogGraph = (props: Props) => {
 
   const [data, setData] = useState([] as vLogWeekly[])
-  const [nodeHostName, setNodeHostName] = useState(window.location.hostname.includes('rixdata.net') || true ? 'stated.gritapp.info' : 'stated.rixdata.net')
+  const [nodeHostName, setNodeHostName] = useState(window.location.hostname.includes('rixdata.net') ? 'stated.gritapp.info' : 'stated.rixdata.net')
 
   useEffect(() => {
     getVerificationLog(props.hash, (data) => {
@@ -79,38 +79,38 @@ const VerificationLogGraph = (props: Props) => {
       <h3>Statement Verification Log</h3>
       <div>
         <TextField label="Monitoring node hostname" value={nodeHostName} onChange={(e) => setNodeHostName(e.target.value)} />
-        {!(data && data[0]) ? (<div style={{padding: '24px'}}>no verification log data</div>)
+        {!(data && data[0]) ? (<div style={{ padding: '24px' }}>no verification log data</div>)
           : (
-            <div style={{padding: '24px', ...(props.lt850px ? {paddingLeft: '0px', paddingRight: '0px'}: {})}}>
-                <LineChart
-                  xAxis={[
-                    {
-                      data: data.map(i => new Date(i.t)).slice(0, 120),
-                      scaleType: 'time', label: 'Time', valueFormatter
-                    },
-                  ]}
-                  series={[
-                    { data: data.map(i => i.api / i.count).slice(0, 120), label: 'Stated API verification success rate', showMark: false },
-                    { data: data.map(i => i.dns / i.count).slice(0, 120), label: 'DNS TXT verification success rate', showMark: false },
-                    { data: data.map(i => i.txt / i.count).slice(0, 120), label: 'Text file verification success rate', showMark: false },
+            <div style={{ padding: '24px', ...(props.lt850px ? { paddingLeft: '0px', paddingRight: '0px' } : {}) }}>
+              <LineChart
+                xAxis={[
+                  {
+                    data: data.map(i => new Date(i.t)).slice(0, 120),
+                    scaleType: 'time', label: 'Time', valueFormatter
+                  },
+                ]}
+                series={[
+                  { data: data.map(i => i.api / i.count).slice(0, 120), label: 'Stated API verification success rate', showMark: false },
+                  { data: data.map(i => i.dns / i.count).slice(0, 120), label: 'DNS TXT verification success rate', showMark: false },
+                  { data: data.map(i => i.txt / i.count).slice(0, 120), label: 'Text file verification success rate', showMark: false },
 
-                  ]}
-                  tooltip={{ trigger: 'axis' }}
-                  width={props.lt850px? 0.8 * window.screen.width : 500}
-                  height={300}
+                ]}
+                tooltip={{ trigger: 'axis' }}
+                width={props.lt850px ? 0.8 * window.screen.width : 500}
+                height={300}
 
-                  legend={{
-                    direction: "column",
-                    position: {
-                      vertical: "top",
-                      horizontal: "middle"
-                    }
-                  }}
-                  sx={{
-                    "--ChartsLegend-rootOffsetX": "-80px",
-                  }}
-                />
-              </div>
+                legend={{
+                  direction: "column",
+                  position: {
+                    vertical: "top",
+                    horizontal: "middle"
+                  }
+                }}
+                sx={{
+                  "--ChartsLegend-rootOffsetX": "-80px",
+                }}
+              />
+            </div>
           )}
       </div>
     </>)
