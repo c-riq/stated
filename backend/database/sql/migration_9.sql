d308d72924c1573769363780f4aa3a2eea9cbc0f
Chris Rieckmann <chrisrieck6@gmail.com>
2023-11-02 15:54:02 +0100

    Change order

--- backend/database/sql/migration_9.sql
+++ backend/database/sql/migration_9.sql
@@ -1,4 +1,4 @@
 ALTER TABLE organisation_verifications 
-ADD column confidence DOUBLE PRECISION;
-ALTER TABLE organisation_verifications 
 ADD column department VARCHAR(100);
+ALTER TABLE organisation_verifications 
+ADD column confidence DOUBLE PRECISION;
