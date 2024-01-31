0807666c6696fecc286f7ce96cea57fb62bb3bcc
Chris Rieckmann <chrisrieck6@gmail.com>
2024-01-31 11:44:10 +0100

    Set email as default publication methot

--- frontend/src/components/GenerateStatement.tsx
+++ frontend/src/components/GenerateStatement.tsx
@@ -9,30 +9,32 @@ type props = {
     prepareStatement: prepareStatement
 }
 
-const GenerateStatement = (props:props) => {
+const PublishStatement = (props:props) => {
     const [showAdditionalOptions, setShowAdditionalOptions] = React.useState(false);
     return (
         <React.Fragment>
             <div style={{textAlign: "left", marginTop: "16px"}}>Time: {props.serverTime.toUTCString()}</div>
             <div style={{display: "flex", flexDirection:"row", flexWrap: "wrap"}}>
-                <Button variant="contained" onClick={() => props.prepareStatement({method: 'static'})}
-                    disabled={!props.authorDomain}
-                    sx={{margin: "12px", flexGrow: 1, minWidth: "200px"}}>
-                    Authenticate by publishing a file on {props.authorDomain || 'your website'}
-                </Button>
-                {showAdditionalOptions ? (<>
-                {props.authorDomain && (<Button variant="contained" onClick={() => props.prepareStatement({method: 'dns'})}
-                    sx={{margin: "12px", flexGrow: 1, minWidth: "200px"}}>
-                    Authenticate by adding a DNS record on {props.authorDomain}
-                </Button>)}
-                <Button variant="contained" onClick={() => props.prepareStatement({method: 'api'})}
-                    sx={{margin: "12px",flexGrow: 1, minWidth: "200px"}} data-testid="publish-using-api-key">
-                    Publish using an API key for {window.location.hostname}
-                </Button>
                 <Button variant="contained" onClick={() => props.prepareStatement({method: 'represent'})}
                     sx={{margin: "12px",flexGrow: 1, minWidth: "200px"}}>
                     Ask {window.location.hostname} to publish for you via email
                 </Button>
+                {showAdditionalOptions ? (<>
+                    {props.authorDomain && (<>
+                        <Button variant="contained" onClick={() => props.prepareStatement({method: 'static'})}
+                            disabled={!props.authorDomain}
+                            sx={{margin: "12px", flexGrow: 1, minWidth: "200px"}}>
+                            Authenticate by publishing a file on {props.authorDomain || 'your website'}
+                        </Button>
+                        <Button variant="contained" onClick={() => props.prepareStatement({method: 'dns'})}
+                            sx={{margin: "12px", flexGrow: 1, minWidth: "200px"}}>
+                            Authenticate by adding a DNS record on {props.authorDomain}
+                        </Button>
+                    </>)}
+                    <Button variant="contained" onClick={() => props.prepareStatement({method: 'api'})}
+                        sx={{margin: "12px",flexGrow: 1, minWidth: "200px"}} data-testid="publish-using-api-key">
+                        Publish using an API key for {window.location.hostname}
+                    </Button>
                 </>) : (props.authorDomain && (<Button onClick={() => setShowAdditionalOptions(true)}
                     sx={{margin: "12px", flexGrow: 1, minWidth: "200px"}} data-testid="show-additional-options">
                     Show additional options
@@ -42,4 +44,4 @@ const GenerateStatement = (props:props) => {
     )
 }
 
-export default GenerateStatement
+export default PublishStatement
