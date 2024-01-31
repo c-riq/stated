e6291f0a405bc825c6b072642bb3181936bc54c6
Chris Rieckmann <chrisrieck6@gmail.com>
2024-01-31 14:13:36 +0100

    Improve publication request email

--- frontend/src/components/generateEmail.ts
+++ frontend/src/components/generateEmail.ts
@@ -2,13 +2,16 @@
 export const generateEmail = ({statement, hash}:{statement:string, hash:string}) => {
     const email = `stated@${window.location.host.replace('stated.','')}`
     const urlEncodedSubject = encodeURIComponent('Publish a statement')
-    const intro = 'Please distribute the following statement on our behalf:'
-    const appendix = 'To increase your confidence rating, we provide evidence below which '+
-    '\n\t (1) show that the website domain name of this email account belongs to the author of the statement such as'+
-    '\n\t\t (1.1) an official document linking the domain and organisation identiy or'+
-    '\n\t\t (1.2) a link to an stated organisation verification statment,'+
-    '\n\t (2) show that the owner of this email is authorized to represent our organisation.'
-    const urlEncodedbody = encodeURIComponent(`${intro}\n\n\n${statement}\n\n\nhash: ${hash}\n\n\n${appendix}`)
+    const publishingRequest = 'Please publish the following statement on our behalf:'
+    const verificationRequest = 'Please also publish a verification of us. ' +
+    'Below we provide evidence to authenticate this request, which '+
+    '\n\t (1) show that the domain name of this email account belongs to the author of the statement such as'+
+    '\n\t\t - an official document containing identity information about my organisation'+
+    '\n\t\t - a document or reference linking the domain and my organisations identiy'+
+    '\n\t (2) show that I am authorized to represent my organisation.'
+    const redactedStatement = statement.replace(/Publishing domain: [^\n]+\n/g, '')
+    const text = `${publishingRequest}\n\n\n${redactedStatement}\n\n\n${verificationRequest}`
+    const urlEncodedbody = encodeURIComponent(text)
     const href = `mailto:${email}?subject=${urlEncodedSubject}&body=${urlEncodedbody}`
     console.log(href)
     window.location.href = href
